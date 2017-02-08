dc6e281102495a5d363f55114034f7ef25661710
Alana Alexander <laninsky@gmail.com>
2017-02-08 17:25:04 -0600

    Update fst_dxy.R

--- fst_dxy.R
+++ fst_dxy.R
@@ -53,6 +53,9 @@ for (i in 1:(dim(catalog)[1])) {
    }
   locustemp <- matrix(c(locus_name,locus_length),nrow=1)
   for (j in 1:(length(no_pops)-1)) {
+    fst <- NA
+    dxy <- NA
+    dstd <- NA
     j_haplotypes <- base_changes[(which(base_changes[,1] %in% pop_map[(which(pop_map[,2]==no_pops[j])),1])),2:(dim(base_changes)[2])]
     if (dim(base_changes)[2]==2) {
       j_haplotypes <- matrix(j_haplotypes,ncol=1)
@@ -65,59 +68,64 @@ for (i in 1:(dim(catalog)[1])) {
       j_haplotypes <- matrix(j_haplotypes,ncol=1)
     }
     j_nos <- dim(j_haplotypes)[1]
-    j_uniques <- unique(j_haplotypes)
-    j_Hs <- 0
-    for (k in 1:(dim(j_uniques)[1])) {
-      j_Hs <- j_Hs + (sum(paste(j_uniques[k,], collapse="")==apply(j_haplotypes,1,paste,collapse=""))/j_nos)^2
-    }
-    j_Hs <- 1-j_Hs
-    for (m in (j+1):(length(no_pops))) {
-      m_haplotypes <- base_changes[(which(base_changes[,1] %in% pop_map[(which(pop_map[,2]==no_pops[m])),1])),2:(dim(base_changes)[2])]
-      if (dim(base_changes)[2]==2) {
-        m_haplotypes <- matrix(m_haplotypes,ncol=1)
+    if(j_nos>0) {
+      j_uniques <- unique(j_haplotypes)
+      j_Hs <- 0
+      for (k in 1:(dim(j_uniques)[1])) {
+        j_Hs <- j_Hs + (sum(paste(j_uniques[k,], collapse="")==apply(j_haplotypes,1,paste,collapse=""))/j_nos)^2
       }
-      remove <- (unique(which(m_haplotypes=="0",arr.ind=TRUE)[,1]))
-      if(length(remove)>0) {
-        m_haplotypes <- m_haplotypes[-remove,]
-      }
-      if (dim(base_changes)[2]==2) {
-        m_haplotypes <- matrix(m_haplotypes,ncol=1)
-      }
-      m_nos <- dim(m_haplotypes)[1]
-      m_uniques <- unique(m_haplotypes)
-      m_Hs <- 0
-      for (k in 1:(dim(m_uniques)[1])) {
-        m_Hs <- m_Hs + (sum(paste(m_uniques[k,], collapse="")==apply(m_haplotypes,1,paste,collapse=""))/m_nos)^2
-      }
-      m_Hs <- 1-m_Hs
-      all_nos <- j_nos + m_nos
-      all_haplotypes <- rbind(j_haplotypes,m_haplotypes)
-      all_uniques <- unique(all_haplotypes)
-      all_Ht <- 0
-      for (k in 1:(dim(all_uniques)[1])) {
-        all_Ht <- all_Ht + (sum(paste(all_uniques[k,], collapse="")==apply(all_haplotypes,1,paste,collapse=""))/all_nos)^2
-      }
-      all_Ht <- 1-all_Ht
-      fst <- (all_Ht-((j_Hs+m_Hs)/2))/all_Ht
-      dxy <- 0
-      for (n in 1:(dim(j_haplotypes)[1])) {
-        for (o in 1:(dim(m_haplotypes)[1])) {
-          dxy <- dxy + (dim(j_haplotypes)[2])-sum(j_haplotypes[n,]==m_haplotypes[o,])
+      j_Hs <- 1-j_Hs
+      for (m in (j+1):(length(no_pops))) {
+        m_haplotypes <- base_changes[(which(base_changes[,1] %in% pop_map[(which(pop_map[,2]==no_pops[m])),1])),2:(dim(base_changes)[2])]
+        if (dim(base_changes)[2]==2) {
+          m_haplotypes <- matrix(m_haplotypes,ncol=1)
         }
-      }
-      if(length(outgroup_haplotypes)>0){
-        dxy_outgroup <- 0
-        for (n in 1:(dim(all_haplotypes)[1])) {
-          for (o in 1:(dim(outgroup_haplotypes)[1])) {
-            dxy_outgroup <- dxy_outgroup + (dim(all_haplotypes)[2])-sum(all_haplotypes[n,]==outgroup_haplotypes[o,])
+        remove <- (unique(which(m_haplotypes=="0",arr.ind=TRUE)[,1]))
+        if(length(remove)>0) {
+          m_haplotypes <- m_haplotypes[-remove,]
+        }
+        if (dim(base_changes)[2]==2) {
+          m_haplotypes <- matrix(m_haplotypes,ncol=1)
+        }
+        m_nos <- dim(m_haplotypes)[1]
+        if(m_nos>0) {
+          m_uniques <- unique(m_haplotypes)
+          m_Hs <- 0
+          for (k in 1:(dim(m_uniques)[1])) {
+            m_Hs <- m_Hs + (sum(paste(m_uniques[k,], collapse="")==apply(m_haplotypes,1,paste,collapse=""))/m_nos)^2
+          }
+          m_Hs <- 1-m_Hs
+          all_nos <- j_nos + m_nos
+          all_haplotypes <- rbind(j_haplotypes,m_haplotypes)
+          all_uniques <- unique(all_haplotypes)
+          all_Ht <- 0
+          for (k in 1:(dim(all_uniques)[1])) {
+            all_Ht <- all_Ht + (sum(paste(all_uniques[k,], collapse="")==apply(all_haplotypes,1,paste,collapse=""))/all_nos)^2
+          }
+          all_Ht <- 1-all_Ht
+          fst <- (all_Ht-((j_Hs+m_Hs)/2))/all_Ht
+          dxy <- 0
+          for (n in 1:(dim(j_haplotypes)[1])) {
+            for (o in 1:(dim(m_haplotypes)[1])) {
+              dxy <- dxy + (dim(j_haplotypes)[2])-sum(j_haplotypes[n,]==m_haplotypes[o,])
+            }
+          }
+          if(length(outgroup_haplotypes)>0){
+            dxy_outgroup <- 0
+            for (n in 1:(dim(all_haplotypes)[1])) {
+              for (o in 1:(dim(outgroup_haplotypes)[1])) {
+                dxy_outgroup <- dxy_outgroup + (dim(all_haplotypes)[2])-sum(all_haplotypes[n,]==outgroup_haplotypes[o,])
+              }
+            }
+          dstd <- dxy/dxy_outgroup  
+          } else {
+          dstd <- NA
           }
         }
-      dstd <- dxy/dxy_outgroup  
-      } else {
-      dstd <- NA
       }
-      temp <- matrix(c(fst,dxy,dstd),nrow=1)
-      locustemp <- cbind(locustemp,temp)
+     } 
+     temp <- matrix(c(fst,dxy,dstd),nrow=1)
+     locustemp <- cbind(locustemp,temp)
      }
     }
    outputname <- rbind(outputname,locustemp)
